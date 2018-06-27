115c472bbcbde934dcbe18586ea9a257e88217b9
Dibakar <lits789@gmail.com>
2018-06-27 17:32:23 +0800

    create pv for notebook

--- scripts/create-pv-for-jupyterhub-notebooks.sh
+++ scripts/create-pv-for-jupyterhub-notebooks.sh
@@ -11,7 +11,7 @@ NOTEBOOKS_PATH=/NTU/SPMS/openshift/jupyter/notebooks-$COURSE
 oc process openshift//pv-template-volume-jupyterhub-notebooks \
    -v COURSE_NAME=$COURSE \
    -v NFS_PV_NOTEBOOKS_PATH=$NOTEBOOKS_PATH \
-   -v NFS_PV_NOTEBOKKS_CAPACITY=25Gi \
+   -v NFS_PV_NOTEBOOKS_CAPACITY=25Gi \
    -v NFS_SERVER_NAME=sds.ntu.edu.sg | oc create -f -
 
 
