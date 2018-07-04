9cae922a64be543b53e8c0265f1a6922c9ce903e
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-07-04 15:12:12 +1000

    Fix session count.

--- scripts/spawn-jupyter-notebooks.sh
+++ scripts/spawn-jupyter-notebooks.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 SERVER=$1
-SESSIONS=$2
-DELAY=$3
+SESSIONS=${2:-5}
+DELAY=${3:-5}
 
-for (( i=0; i<=$SESSIONS; i++ )); do
+for (( i=1; i<=$SESSIONS; i++ )); do
     echo "Spawning user #$i."
     rm -f /tmp/jupyterhub-cookies.txt
     curl -sL -c /tmp/jupyterhub-cookies.txt -o /dev/null $SERVER/hub/spawn
