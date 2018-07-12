6c2c7ed39f093e0d3c4859857e1cbe43c12aaa8a
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-07-12 21:09:55 +1000

    Allow all inputs to be supplied as arguments.

--- scripts/deploy-jupyterhub.sh
+++ scripts/deploy-jupyterhub.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# All inputs need to be provided through prompts.
+# Script can optionally be passed the arguments. If not supplied the
+# user will be prompted to supply them.
 
-read -p "Course Name: " COURSE_NAME
+if [ "$#" -ge 1 ]; then
+    COURSE_NAME=$1
+    shift
+else
+    read -p "Course Name: " COURSE_NAME
+fi
 
 COURSE_NAME=`echo $COURSE_NAME | tr 'A-Z' 'a-z'`
 
@@ -11,22 +17,52 @@ if ! [[ $COURSE_NAME =~ ^[a-z0-9-]*$ ]]; then
     exit 1
 fi
 
-read -p "Notebook Repository URL: " NOTEBOOK_REPOSITORY_URL
-read -p "Notebook Repository Context Dir: " NOTEBOOK_REPOSITORY_CONTEXT_DIR
-read -p "Notebook Repository Reference [master]: " NOTEBOOK_REPOSITORY_REFERENCE
-
-if [ "$NOTEBOOK_REPOSITORY_REFERENCE" = "" ]; then
-    NOTEBOOK_REPOSITORY_REFERENCE=master
+if [ "$#" -ge 1 ]; then
+    NOTEBOOK_REPOSITORY_URL=$1
+    shift
+else
+    read -p "Notebook Repository URL: " NOTEBOOK_REPOSITORY_URL
 fi
 
-read -p "LDAP Search User: " LDAP_SEARCH_USER
-read -s -p "LDAP Search Password: " LDAP_SEARCH_PASSWORD
+if [ "$#" -ge 1 ]; then
+    NOTEBOOK_REPOSITORY_CONTEXT_DIR=$1
+    shift
+else
+    read -p "Notebook Repository Context Dir: " NOTEBOOK_REPOSITORY_CONTEXT_DIR
+fi
 
-echo
+if [ "$#" -ge 1 ]; then
+    NOTEBOOK_REPOSITORY_REFERENCE=$1
+    shift
+else
+    read -p "Notebook Repository Reference [master]: " NOTEBOOK_REPOSITORY_REFERENCE
+fi
 
-read -p "JupyterHub Admin Users: " JUPYTERHUB_ADMIN_USERS
+if [ "$#" -ge 1 ]; then
+    LDAP_SEARCH_USER=$1
+    shift
+else
+    read -p "LDAP Search User: " LDAP_SEARCH_USER
+fi
 
-read -p "Continue? [Y/n] " DO_UPDATE
+if [ "$#" -ge 1 ]; then
+    LDAP_SEARCH_PASSWORD=$1
+    shift
+else
+    read -s -p "LDAP Search Password: " LDAP_SEARCH_PASSWORD
+    echo
+fi
+
+if [ "$#" -ge 1 ]; then
+    JUPYTERHUB_ADMIN_USERS=$1
+    shift
+else
+    read -p "JupyterHub Admin Users: " JUPYTERHUB_ADMIN_USERS
+fi
+
+if [ x"$CONTINUE_PROMPT" != x"n" ]; then
+    read -p "Continue? [Y/n] " DO_UPDATE
+fi
 
 if ! [[ $DO_UPDATE =~ ^[Yy]?$ ]]; then
     exit 1
